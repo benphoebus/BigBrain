3a0204cdb317e69dc979f6e227c3cabfda17d685
Ben Phoebus <z5313164@student.unsw.edu.au>
2020-12-22 17:23:34 +1100

    minor tweak to icons

--- frontend/src/components/SidebarData.jsx
+++ frontend/src/components/SidebarData.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 // import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-// import * as IoIcons from 'react-icons/io';
+import * as IconName from 'react-icons/vsc';
 
 const SidebarData = [
   {
-    title: 'Home',
-    path: '/',
+    title: 'Login',
+    path: '/Login',
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text',
   },
   {
-    title: 'Feedback',
-    path: '/Feedback',
-    icon: <AiIcons.AiFillHome />,
+    title: 'Register',
+    path: '/Register',
+    icon: <IconName.VscAccount />,
     cName: 'nav-text',
   },
 ];
