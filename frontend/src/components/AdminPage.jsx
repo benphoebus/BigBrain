b14532c77d81f1ef7b35a2843fbd8534f13d780f
Ben Phoebus <z5313164@student.unsw.edu.au>
2021-01-25 13:43:59 +1100

    quizlist working on login and refresh of admin page

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import QuizListCard from './QuizListCard';
 
 function AdminPage() {
+  const [quizList, setQuizList] = useState([]);
+  const [noQuizzes, setNoQuizzes] = useState(false);
+
+  // When page refreshes get all quizzes
+  useEffect(() => {
+    const listOfQuizzes = [];
+    const token = localStorage.getItem('token');
+    // eslint-disable-next-line
+    console.log(' my token:: ', token);
+    const result = axios({
+      method: 'get',
+      url: 'http://127.0.0.1:5005/admin/quiz',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      if (response.data.quizzes.length === 0) {
+        setNoQuizzes(!false);
+      } else {
+        setNoQuizzes(false);
+        const { quizzes } = response.data;
+        // eslint-disable-next-line
+        console.log(quizzes);
+        for (let i = 0; i < response.data.quizzes.length; i += 1) {
+          listOfQuizzes[i] = response.data.quizzes[i];
+        }
+        setQuizList(listOfQuizzes);
+      }
+    }).catch((err) => {
+      // eslint-disable-next-line
+      console.log(err);
+    });
+    // eslint-disable-next-line
+    console.log(result);
+  }, []);
+
   return (
     <>
-      Welcome to the Dashboard.
-      <QuizListCard />
+      <h3>Welcome to the Dashboard.</h3>
+      {noQuizzes ? (
+        <>
+          <h3>Welcome to the Dashboard.</h3>
+          <h3>Create your own quiz</h3>
+        </>
+      ) : (
+        null
+      )}
+      {quizList.map((quiz) => (
+        <QuizListCard
+          quiz={quiz}
+        />
+      ))}
     </>
   );
 }
--- frontend/src/components/QuizListCard.jsx
+++ frontend/src/components/QuizListCard.jsx
@@ -1,56 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest craco+eslint or Next.js?
 import { Card } from 'react-bootstrap';
 // import PropTypes from 'prop-types';
 
-function QuizListCard() {
-  const [data, setData] = useState({ quiz: [] });
-
-  // When page refreshes get all quizzes
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    // eslint-disable-next-line
-    console.log(token);
-    const result = axios({
-      method: 'get',
-      url: 'http://127.0.0.1:5005/admin/quiz',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((response) => {
-      // eslint-disable-next-line
-      console.log(response);
-      const { quizzes } = response.data;
-      // eslint-disable-next-line
-      console.log(quizzes);
-    }).catch((err) => {
-      // eslint-disable-next-line
-      console.log(err);
-    });
-    setData(result.data);
-  }, []);
-
-  // eslint-disable-next-line
-  console.log(data)
-
+function QuizListCard({ quiz }) {
   return (
-    <>
-      {/* {data.quiz.forEach((quiz) => { */}
-      <Card className="my-3 p-3 rounded">
-        {/* <Card.Img src={quiz.thumbnail} variant="top" /> */}
-        <Card.Body>
-          <Card.Title as="div">
-            {/* <strong>{quiz.name}</strong> */}
-          </Card.Title>
-          <Card.Text as="span">
-            {/* {quiz.createdAt} */}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      {/* })} */}
-    </>
+    <Card className="my-3 p-3 rounded">
+      <Card.Img src={quiz.thumbnail} variant="top" />
+      <Card.Body>
+        <Card.Title as="div">
+          <strong>{quiz.name}</strong>
+        </Card.Title>
+        <Card.Text as="span">
+          {quiz.createdAt}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
